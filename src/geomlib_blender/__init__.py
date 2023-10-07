c2b996e505aa5d080634ddbe9704696a1cfff37e
kyjohnso <kyjohnso@gmail.com>
2023-10-07 16:20:00 +0100

    initial commit and build stuff

---
+++ src/geomlib_blender/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from .geomlib_blender import *
+
+version = "0.1.0"
---
+++ src/geomlib_blender/geomlib_blender.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import numpy as np
+import bpy
+import mathutils
+
+def create_3d_ellipsoid_from_covariance(covariance,factor=1):
+    """
+    Creates a blender 3d ellipsoid from a 3d covariance matrix rotating and scaling
+    it in the scene appropriately.
+
+    Arguments:
+        covariance: a np.ndarray of shape 3x3 representing the covariance of an ellipsoid
+        factor: a scale factor to multiply the size of the ellipse by
+    """
+    val, vec = np.linalg.eig(covariance)
+
+    mat = np.eye(4)
+    mat[:3,:3] = vec*np.sqrt(val[np.newaxis,:])
+    mat = mathutils.Matrix(mat)
+
+    bpy.ops.mesh.primitive_ico_sphere_add(
+        subdivisions=4,
+        radius=1,
+        enter_editmode=False,
+        align="WORLD",
+        location=(0,0,0),
+        scale = (1,1,1),
+    )
+    bpy.context.object.matrix_world = mat
+
+    return bpy.context.object
